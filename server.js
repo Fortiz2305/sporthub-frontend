926681d0f088be194e8590ebdd5338bcf4ee9553
Francisco Ortiz <fortizabril@gmail.com>
2017-08-25 13:16:36 +0200

    Fix route refresh

--- server.js
+++ server.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
 
 app.use(express.static('static'));
 app.use(express.static('dist'));
+
+app.get('/*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'static', 'index.html'));
+});
 
 app.listen(8080, () => console.log('You can access your app in http://localhost:8080'));
